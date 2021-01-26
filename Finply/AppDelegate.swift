12df0f37231db88bbd923f1916cb80985b70b1a5
illiapostoienko <postoienkoilyha@gmail.com>
2021-01-26 16:53:42 +0200

    Check 2

--- Finply/AppDelegate.swift
+++ Finply/AppDelegate.swift
@@ -22,7 +22,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+        
+      return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {}
